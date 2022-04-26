50bf86c74cd8ca4b21ac2a5d3750bac519c419fe
Everth Paredes <epe0413@gmail.com>
2022-04-26 10:42:05 -0600

    Se agrego componente de NuevoPresupuesto con su validación y un componente de Mensaje reutilizable

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+import Mensaje from './Mensaje'
+
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+  const [mensaje, setMensaje]= useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+
+    if(!Number(presupuesto) || Number(presupuesto) < 0) {
+      setMensaje('No es un presupuesto valido')
+    } else {
+      console.log('Si es un numero')
+    }
+  }
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
+            <div className='campo'>
+                <label>Definir Presupuesto</label>
+                <input
+                    className='nuevo-presupuesto'
+                    type="text"
+                    placeholder='Añade tu Presupuesto'
+                    value= {presupuesto}
+                    onChange = { e => setPresupuesto(e.target.value)}
+                />
+            </div>
+
+            <input type="submit" value="Añadir"></input>
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto

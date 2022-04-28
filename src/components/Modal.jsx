d2a5c20cb20cccade5d1f1744c95acc63033ec41
Everth Paredes <epe0413@gmail.com>
2022-04-28 15:04:53 -0600

    Se agregó el cuerpo del formulario

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,14 @@
 import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal, animarModal,setAnimarModal}) => {
 
     const ocultarModal = () => {
-        setModal(false)
+        
+        setAnimarModal(false)
+
+        setTimeout(() => {
+            setModal(false)
+        }, 500);
     }
   return (
     <div className="modal">
@@ -14,6 +19,51 @@ const Modal = ({setModal}) => {
                 onClick={ocultarModal}
             />
         </div>
+
+        <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
+            <legend>Nuevo Gasto</legend>
+            <div className='campo'>
+                <label htmlFor="nombre">Nombre Gasto</label>
+                <input
+                    id='nombre'
+                    type='text'
+                    placeholder='Añade el Nombre del Gasto'
+                />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor="cantidad">Cantidad</label>
+                <input
+                    id='cantidad'
+                    type='text'
+                    placeholder='Añade la cantidad del gasto. Ej: 300'
+                />
+            </div>
+
+            <div className='campo'>
+                <label htmlFor="categoria">Categoria</label>
+
+                <select
+                    id='categoria'
+                >
+                    <option value="">--Seleccione--</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos">Gastos Varios</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="salud">Salud</option>
+                    <option value="suscripciones">Suscripciones</option>
+
+                </select>
+                
+            </div>
+
+            <input
+                type='submit'
+                value='Añadir Gasto'
+            />
+        </form>
     </div>
   )
 }

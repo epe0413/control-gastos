87579d1299f8d38ec6ae5eed8db96b2b3e22aad2
Everth Paredes <epe0413@gmail.com>
2022-05-06 09:47:21 -0600

    Se agrego funcionalidad para resetear la app

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 
-const ControlPresupuesto = ({gastos, presupuesto}) => {
+const ControlPresupuesto = ({gastos,setGastos, presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
 
     const [disponible, setDisponible] = useState(0)
     const [gastado, setGastado] = useState(0)
@@ -30,6 +30,16 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
         })
     }
 
+    const handleResetApp = () => {
+        const resultado = confirm('¿Desea reiniciar presupuesto y gastos?');
+
+        if (resultado){
+            setGastos([])
+            setPresupuesto(0)
+            setIsValidPresupuesto(false)
+        }
+    }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         <div>
@@ -44,6 +54,13 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
             />
         </div>
         <div className='contenido-presupuesto'>
+            <button
+                className='reset-app'
+                type='button'
+                onClick={handleResetApp}
+            >
+                Resetear App
+            </button>
             <p>
                 <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
             </p>
